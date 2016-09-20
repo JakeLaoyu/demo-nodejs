afe49952eaaff75986a06521a83ff31547734cf3
Jake <Jake_Laoyu@163.com>
2016-09-20 09:44:13 +0800

    update

--- helloworld.js
+++ helloworld.js
@@ -1,33 +1,46 @@
-var express = require('express');
+var eventproxy = require('eventproxy');
 var superagent = require('superagent');
 var cheerio = require('cheerio');
+var url = require('url');
 
+var cnodeUrl = 'https://cnodejs.org/';
 
-var app = express();
+superagent.get(cnodeUrl)
+  .end(function (err, res) {
+    if (err) {
+      return console.error(err);
+    }
+    var topicUrls = [];
+    var $ = cheerio.load(res.text);
+    $('#topic_list .topic_title').each(function (idx, element) {
+      var $element = $(element);
+      var href = url.resolve(cnodeUrl, $element.attr('href'));
+      topicUrls.push(href);
+    });
 
-app.get('/', function(req, res, next) {
-    // 用 superagent 去抓取 https://cnodejs.org/ 的内容
-    superagent.get('http://mood.jakexin.top/')
-        .end(function(err, sres) {
-            // 常规的错误处理
-            if (err) {
-                return next(err);
-            }
-            // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
-            // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-            // 剩下就都是 jquery 的内容了
-            var $ = cheerio.load(sres.text);
-            var items = [];
-            $('.index-title a').each(function(index, element) {
-                var $element = $(element);
-                items.push({
-                    // title: $element.html(),
-                    href: $element.attr('href')
-                });
-            });
+    var ep = new eventproxy();
 
-            res.send(items);
+    ep.after('topic_html', topicUrls.length, function (topics) {
+      topics = topics.map(function (topicPair) {
+        var topicUrl = topicPair[0];
+        var topicHtml = topicPair[1];
+        var $ = cheerio.load(topicHtml);
+        return ({
+          title: $('.topic_full_title').text().trim(),
+          href: topicUrl,
+          comment1: $('.reply_content').eq(0).text().trim(),
         });
-}).listen(3000, function() {
-    console.log('http://localhost:3000')
-});
+      });
+
+      console.log('final:');
+      console.log(topics);
+    });
+
+    topicUrls.forEach(function (topicUrl) {
+      superagent.get(topicUrl)
+        .end(function (err, res) {
+          console.log('fetch ' + topicUrl + ' successful');
+          ep.emit('topic_html', [topicUrl, res.text]);
+        });
+    });
+  });
