d32eba2b691cec71d507034ce4ad1f83a38e0d26
Jake <Jake_Laoyu@163.com>
2016-10-23 21:28:45 +0800

    update

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-var buf = new Buffer([10, 20, 30, 40, 50]);
+var buffer1 = new Buffer('ABC');
+// 拷贝一个缓冲区
+var buffer2 = new Buffer('dsfds');
+buffer1.copy(buffer2,0,0,20);
 
-console.log(buf);
+
+console.log(buffer1.length);
+
+console.log("buffer2 content: " + buffer2.toString());
