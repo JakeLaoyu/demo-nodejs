66876906d872089b0d4cbac0cdb86efa0661e5d8
Jake <Jake_Laoyu@163.com>
2016-10-25 22:54:13 +0800

    update

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-var buffer1 = new Buffer('ABC');
-// 拷贝一个缓冲区
-var buffer2 = new Buffer('dsfds');
-buffer1.copy(buffer2,0,0,20);
+var hello = require('./hello.js');
 
-
-console.log(buffer1.length);
-
-console.log("buffer2 content: " + buffer2.toString());
+hello.world();
