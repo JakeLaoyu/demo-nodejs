da86ff216bfe6de221f5b21c62d182ba3155e7fd
Jake <Jake_Laoyu@163.com>
2016-12-16 22:13:14 +0800

    email

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
-var server = require("./app");
-var router = require("./router");
+var nodemailer = require('nodemailer');
 
-server.start(router.route);
+var transporter = nodemailer.createTransport('SMTP', {
+	service: 'smtp.qq.com',
+	auth: {
+		user: '944473232@qq.com',
+		pass: 'gentleman3232.'
+	},
+});
+
+var mailOptions = {
+	from: '944473232@qq.com', // sender address
+	to: 'Jake_Laoyu@163.com', // list of receivers
+	subject: 'Hello ✔', // Subject line
+	text: 'Hello world ✔', // plaintext body
+	html: '<b>Hello world ✔</b>' // html body
+};
+
+transporter.sendMail(mailOptions, function(error, info) {
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Message sent: ' + info.response);
+	}
+});
