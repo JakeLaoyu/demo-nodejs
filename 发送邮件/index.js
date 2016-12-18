18689de06e1f1863b0b18da0d31f1a6cdbd5b2d6
Jake <Jake_Laoyu@163.com>
2016-12-18 12:05:42 +0800

    email

--- 发送邮件/index.js
+++ 发送邮件/index.js
@@ -5,17 +5,17 @@ var transporter = nodemailer.createTransport({
 	secureConnection: true,
 	port: 25,
 	auth: {
-		user: 'jake.laoyu@qq.com',
-		pass: 'bnqusyosulvsbcje'
+		user: '944473232@qq.com',
+		pass: '***' //授权码
 	}
 });
 
 var mailOptions = {
-	from: 'Jake <jake.laoyu@qq.com>', // sender address
+	from: '944473232@qq.com', // sender address
 	to: 'Jake_Laoyu@163.com', // list of receivers
-	subject: '1', // Subject line
-	// text: '', // plaintext body
-	html: '<p>您提交的展示项目审批通过，赶紧点击看看吧去看看吧！！点击<a href="http://i.jakeyu.top">机票</a>前往</p>' // html body
+	subject: 'Hello ✔', // Subject line
+	text: 'Hello world ✔', // plaintext body
+	html: '<b>Hello world ✔</b>' // html body
 };
 
 
